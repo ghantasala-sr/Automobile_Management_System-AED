ad39aa6566ec4850158ba2e0b6bb817970e4eb1b
ghantasala-sr <ghantasala.s@northeastern.edu>
2024-12-08 20:32:47 -0500

    Car Directory

---
+++ Automobile_Project/src/Business/Car/InventoryDirectory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Car;
+
+/**
+ *
+ * @author srinivasarithikghantasala
+ */
+public class InventoryDirectory {
+    
+    private Car car;
+    private int quantity;
+    private String date;
+    
+}
